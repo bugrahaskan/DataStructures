9c2a899ad8f49bc21128137df3903678eca9723b
Buğra Haskan <bugrahaskan@outlook.com>
2022-11-03 14:54:41 +0300

    linked list

--- LinkedList.py
+++ LinkedList.py
@@ -1,6 +1,3 @@
-from lib2to3.pytree import Node
-
-
 from Node import Node
 
 class LinkedList:
@@ -10,5 +7,47 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
 my_linked_list = LinkedList(4)
-print(my_linked_list.head.value)
+
+
+#my_linked_list.print_list()
+my_linked_list.append(2)
+my_linked_list.print_list()
